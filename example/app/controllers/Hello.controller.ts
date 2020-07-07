adacee0d05355b0353ea3904dc7393ff36ca546b
Matheus Vinicius <mtwzim@gmail.com>
2020-07-06 23:03:12 -0300

    Routing system and middleware

---
+++ example/app/controllers/Hello.controller.ts
@@ -0,0 +1,7 @@
+class Hello {
+  world({response}: any) {
+    response.body = "Hello world!"
+  }
+}
+
+export default new Hello()
---
+++ example/routes/web.ts
@@ -0,0 +1,15 @@
+import {Router} from '../../mod.ts'
+import { TestMiddleware } from '../app/middlewares/Test.middleware.ts'
+import HelloController from '../app/controllers/Hello.controller.ts'
+
+/**
+ * Routing Web
+ * 
+ * Example:
+ *  Router.get('router', ({request, response}) => {});
+ */
+
+
+Router.use(TestMiddleware).get('/', HelloController.world)
+
+export default Router
